e0e239b03cc050068599eff4dd7acf6be7a93acf
Leonchang06 <leoncrh-wm24@student.tarc.edu.my>
2025-02-22 20:13:37 +0800

    App

--- Leon.Practical/src/App.java
+++ Leon.Practical/src/App.java
@@ -1,5 +1,13 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.printf("%-12s %-12s %-12s\n", "Number", "Squareroot", "Cube");
+
+        for (int i = 0; i <= 20; i += 2) {
+            System.out.printf("%-12d %-12.4f %-12.4f\n", i, Math.sqrt(i), Math.pow(i, 3));
+
+        }
+
     }
 }
